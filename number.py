19c9fd05146577a7db5283789015e8418a75ad49
SurajChadha001 <surajchadhamca@gmail.com>
2024-08-12 09:33:23 +0530

    commit

---
+++ number.py
@@ -0,0 +1,7 @@
+cucumbers = 5
+price_per_cucumbers = 3.25
+total_cost = cucumbers * price_per_cucumbers
+price_per_cucumbers = 3.25
+total_cost = cucumbers * price_per_cucumbers
+print(total_cost)
+print(type(total_cost))
