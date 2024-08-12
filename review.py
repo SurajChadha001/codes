668922b710720901245257ff2217d7363338e27c
SurajChadha001 <surajchadhamca@gmail.com>
2024-08-12 11:00:54 +0530

    Update review.py

---
+++ review.py
@@ -0,0 +1,12 @@
+skill_completed  = "Python Syntax"
+
+exercises_completed = 13
+points_per_exercise = 5
+
+poin_total = 100
+
+poin_total = poin_total + (exercises_completed * points_per_exercise)
+
+# The amount of points for each exercise may change because points don't exist yet
+
+print("I got",print_total,"points!")
